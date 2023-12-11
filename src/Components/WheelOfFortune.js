9fcb238511356f7a55ad897a86a5e1d2958ca274
VadimBestSorka <113136826+BrainHew@users.noreply.github.com>
2023-12-11 20:14:45 +0300

    create project

---
+++ src/Components/WheelOfFortune.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const WheelOfFortune = () => {
+    const [selectedItem, setSelectedItem] = useState(null);
+    const items = ['Александра Федоренко', 'Дарья Недова', 'Элиза Парфомюк', 'Рома Иванов', 'Светлана Сергеевна Панадий'];
+
+    const spinWheel = () => {
+        const randomIndex = Math.floor(Math.random() * items.length);
+        setSelectedItem(items[randomIndex]);
+    };
+
+    return (
+        <div>
+            <h1>Колесо фортуны</h1>
+            {selectedItem ? (
+                <div>
+                    <h2>Вы выиграли: {selectedItem}</h2>
+                </div>
+            ) : (
+                <button onClick={spinWheel}>Крутить колесо</button>
+            )}
+        </div>
+    );
+};
+
+export default WheelOfFortune;
